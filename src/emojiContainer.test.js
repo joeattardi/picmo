65874d5668064dc04bc012ae2a4d900d03afa650
Joe Attardi <joe@attardi.net>
2019-09-14 09:58:11 -0400

    Add tests for EmojiContainer

---
+++ src/emojiContainer.test.js
@@ -0,0 +1,17 @@
+const Emitter = require('tiny-emitter');
+
+const { EmojiContainer } = require('./emojiContainer');
+
+describe('EmojiContainer', () => {
+  test('should render all the given emojis', () => {
+    const emojis = [
+      { e: '⚡️'},
+      { e: '👍'}
+    ];
+
+    const events = new Emitter();
+
+    const container = new EmojiContainer(emojis, false, events).render();
+    expect(container.querySelectorAll('.emoji-picker__emoji').length).toBe(2);
+  });
+});
