ccdc28c766f2b60a80eb626bcdcb10fbac45a0ed
Joe Attardi <joe@attardi.net>
2019-09-14 15:35:26 -0400

    Add tests for Tabs

---
+++ src/tabs.test.js
@@ -0,0 +1,32 @@
+const Emitter = require('tiny-emitter');
+
+const { Tabs } = require('./tabs');
+
+describe('Tabs', () => {
+  function isActive(tab) {
+    return tab.classList.contains('active');
+  }
+
+  test('should set the active tab', () => {
+    const events = new Emitter();
+    const tabsContainer = new Tabs(events).render();
+
+    const tab2 = tabsContainer.querySelectorAll('.emoji-picker__tab')[2];
+    const body2 = tabsContainer.querySelectorAll('.emoji-picker__tab-body')[2];
+    expect(isActive(tab2)).toBe(false);
+    expect(isActive(body2)).toBe(false);
+    tab2.dispatchEvent(new MouseEvent('click'));
+    expect(isActive(tab2)).toBe(true);
+    expect(isActive(body2)).toBe(true);
+
+    const tab0 = tabsContainer.querySelectorAll('.emoji-picker__tab')[0];
+    const body0 = tabsContainer.querySelectorAll('.emoji-picker__tab-body')[0];
+    expect(isActive(tab0)).toBe(false);
+    expect(isActive(body0)).toBe(false);
+    tab0.dispatchEvent(new MouseEvent('click'));
+    expect(isActive(tab0)).toBe(true);
+    expect(isActive(body0)).toBe(true);
+    expect(isActive(tab2)).toBe(false);
+    expect(isActive(body2)).toBe(false);
+  });
+});
