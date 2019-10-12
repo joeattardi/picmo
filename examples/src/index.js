8ef959efc5df28e0829d02ea4d679ce102bacafc
Joe Attardi <joe@attardi.net>
2019-10-12 18:26:32 -0400

    Only add livereload script if running on localhost

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,7 +1,9 @@
 const EmojiButton = require('emoji-button');
 
-document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
-':35729/livereload.js?snipver=1"></' + 'script>');
+if (location.hostname === 'localhost') {
+  document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
+  ':35729/livereload.js?snipver=1"></' + 'script>');
+}
 
 window.addEventListener('DOMContentLoaded', () => {
   const button = document.querySelector('#emoji-button');
