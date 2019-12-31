37cdc8c4319e8bf0f8ce6ebcbdbbdd31f15feb0d
Joe Attardi <jattardi@gmail.com>
2019-12-30 21:54:35 -0500

    Make demo site work in IE11

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,20 +1,19 @@
-const EmojiButton = require('@joeattardi/emoji-button');
+var EmojiButton = require('@joeattardi/emoji-button');
 
 if (location.hostname === 'localhost') {
   document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
   ':35729/livereload.js?snipver=1"></' + 'script>');
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-  const button = document.querySelector('#emoji-button');
-  const picker = new EmojiButton({
-  });
+window.addEventListener('DOMContentLoaded', function () {
+  var button = document.querySelector('#emoji-button');
+  var picker = new EmojiButton();
 
-  picker.on('emoji', emoji => {
+  picker.on('emoji', function (emoji) {
     document.querySelector('input').value += emoji;
   });
 
-  button.addEventListener('click', () => {
+  button.addEventListener('click', function () {
     picker.pickerVisible ? picker.hidePicker() : picker.showPicker(button);
   });
 });
