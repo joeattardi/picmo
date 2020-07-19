a3cef8fd56e15f69f985c107c77fbace378e7c19
Joe Attardi <jattardi@gmail.com>
2020-07-18 20:42:15 -0400

    Add buttons to header

--- site/src/components/Header.js
+++ site/src/components/Header.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import logo from '../images/header.png';
 
 import styles from './Header.module.css';
@@ -17,6 +21,14 @@ export default function Header() {
         </a>
         <a href="https://www.npmjs.com/package/@joeattardi/emoji-button">
           <img src="https://img.shields.io/npm/v/@joeattardi/emoji-button" />
+        </a>
+      </div>
+      <div className={styles.buttons}>
+        <a href="https://github.com/joeattardi/emoji-button">
+          <FontAwesomeIcon icon={faGithub} /> GitHub Repository
+        </a>
+        <a href="/docs">
+          <FontAwesomeIcon icon={faBook} /> Documentation
         </a>
       </div>
     </header>
