b821a313bf03dfdc79da6004a3e55c852d6db2fc
zhuiks <zhuke@ukr.net>
2021-04-19 17:36:27 -0500

    add custom build cldr-json

---
+++ vendor/cldr-json/local-config.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+# which types to generate
+TYPES="annotations annotationsDerived"
+
+# Only these files
+MATCH='de|en|es|fr|nl|ru|sk|uk'
+
+# override the version number of CLDR
+VERSION="39.0"
