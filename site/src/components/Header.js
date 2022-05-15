bc3484e9165257b8439af2149f1ff7903172c79a
Joe Attardi <jattardi@gmail.com>
2022-05-15 10:06:13 -0400

    Add PicMo message

--- site/src/components/Header.js
+++ site/src/components/Header.js
@@ -7,7 +7,6 @@ import { faBook } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { OutboundLink } from 'gatsby-plugin-gtag';
 
-
 import logo from '../images/header.png';
 
 import styles from './Header.module.css';
@@ -15,6 +14,18 @@ import styles from './Header.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
+      <div className={styles.alert}>
+        <h2>
+          🚨 Emoji Button is now{' '}
+          <a href="https://npmjs.com/package/picmo">PicMo</a>!
+        </h2>
+        <p>An all new design, smaller bundle size, and more!</p>
+        <p>
+          This website and documentation will remain, but no further updates are
+          planned for Emoji Button 4.x. Upgrade today! Check it out at{' '}
+          <a href="https://picmojs.com">https://PicmoJS.com</a>.
+        </p>
+      </div>
       <div className={styles.logo}>
         <img src={logo} />
       </div>
