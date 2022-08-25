d56e3173d9293de4446d271584e38d215b71aab6
Joe Attardi <jattardi@gmail.com>
2022-08-25 00:09:43 -0400

    Fix invalid CSS and add minification to build

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,13 @@
 import nesting from 'postcss-nesting';
 import mixins from 'postcss-mixins';
 import postcssImport from 'postcss-import';
+import cssnano from 'cssnano';
 
 export default {
   plugins: [
     postcssImport(),
     mixins(),
     nesting(),
+    cssnano()
   ]
 };
