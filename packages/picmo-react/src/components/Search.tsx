df69ac9769ccd4dfe551429b5f45763b3a828762
Joe Attardi <jattardi@gmail.com>
2022-11-28 01:10:47 -0500

    add simplified i18n and Search skeleton

--- packages/picmo-react/src/components/Search.tsx
+++ packages/picmo-react/src/components/Search.tsx
@@ -1,9 +1,10 @@
 import { useContext } from 'react';
 
 import { PicMoContext } from './PicMoProvider';
+import i18n from '../i18n';
 
 export default function Search() {
   return (
-    <input type="text" />
-  )
+    <input type="text" placeholder={i18n.search.placeholder}/>
+  );
 }
---
+++ packages/picmo-react/src/i18n/index.ts
@@ -0,0 +1,4 @@
+import en from './en.json';
+
+// TODO: multiple locale loading support
+export default en;
