0b1ad3df7311c4cdebc6dbfc956402d66dc92e9d
Henryk Helsky <henryk@rootz.com>
2023-02-23 14:33:31 +0200

    add de,fi,fr,nl,no and se translations

---
+++ packages/picmo/src/i18n/lang-de.ts
@@ -0,0 +1,22 @@
+export default {
+    'categories.activities': 'Aktivitäten',
+    'categories.animals-nature': 'Tiere & Natur',
+    'categories.custom': 'Benutzerdefiniert',
+    'categories.flags': 'Flaggen',
+    'categories.food-drink': 'Essen & Trinken',
+    'categories.objects': 'Gegenstände',
+    'categories.people-body': 'Mensch & Körper',
+    'categories.recents': 'Zuletzt genutzt',
+    'categories.smileys-emotion': 'Smileys & Emotionen',
+    'categories.symbols': 'Symbole',
+    'categories.travel-places': 'Reisen & Orte',
+    'error.load': 'Emojis wurden nicht geladen',
+    'recents.clear': 'Zuletzt genutzte Emojis löschen',
+    'recents.none': 'Kein Emoji ausgewählt.',
+    'retry': 'Erneut versuchen',
+    'search.clear': 'Suche löschen',
+    'search.error': 'Suche erfolglos',
+    'search.notFound': 'Kein Emoji gefunden',
+    'search': 'Emojis durchsuchen ...'
+  };
+  
