ae4431cd22f06a97e530e878bd0db58331817ad4
Joe Attardi <jattardi@gmail.com>
2023-05-02 06:08:20 -0400

    Start popup work

---
+++ src/popup/index.ts
@@ -0,0 +1,3 @@
+export default function createPopup() {
+  // console.log('popup');
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: './src/index.ts',
-      name: 'PicMo',
-      fileName: 'index'
+      formats: ['es'],
+      entry: {
+        index: './src/index.ts',
+        'popup/index': './src/popup/index.ts'
+      },
     }
   },
   plugins: [svelte({
